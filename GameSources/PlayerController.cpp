8d6ffcfe2fb0e7a2de9526d19408b26d67f5d842
Yuma Kokubun <zeal22300066@fsgcl.jp>
2023-03-13 15:37:46 +0900

    PlayerController.hとPlayerController.cppを追加した

---
+++ GameSources/PlayerController.cpp
@@ -0,0 +1,12 @@
+/*!
+@file PlayerController.cpp
+@brief プレイヤーキャラ
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+
+}
---
+++ GameSources/PlayerController.h
@@ -0,0 +1,12 @@
+/*!
+@file PlayerController.h
+@brief プレイヤーキャラ
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross{
+
+
+}
