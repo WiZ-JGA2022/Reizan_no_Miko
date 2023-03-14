da731d89325d2dd708150970b4f8860d7d4f30c2
Tomoki Suzuki <zeal22300014@fsgcl.jp>
2023-03-14 14:33:14 +0900

    Expbarのクラス作成

--- GameSources/ExpBar.cpp
+++ GameSources/ExpBar.cpp
@@ -8,6 +8,6 @@
 
 namespace basecross {
 
-
+	
 }
 //end basecross
--- GameSources/ExpBar.h
+++ GameSources/ExpBar.h
@@ -1,13 +1,18 @@
-* !
+/* !
 @file Character.h
 @brief キャラクターなど
-* /
+*/
 
 #pragma once
 #include "stdafx.h"
+#include "Project.h"
 
 namespace basecross {
+	
+	class ExpBar : public GameObject //GameObjectを流用する（継承）
+	{
 
-
+	};
+	
 }
 //end basecross#pragma once
