a8c2af54cc25d52d790272a146c0e7a02bc803ac
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-03-20 13:47:18 +0900

    RandomSelectLevelUpクラスの.hと.cppを追加

---
+++ GameSources/RandomSelectLevelUp.cpp
@@ -0,0 +1,13 @@
+/*!
+@file RandomSelectLevelUp.cpp
+@brief ランダムにレベルアップできる項目を出すクラスの実装
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+
+}
+//end basecross
---
+++ GameSources/RandomSelectLevelUp.h
@@ -0,0 +1,13 @@
+/*!
+@file RandomSelectLevelUp.h
+@brief ランダムにレベルアップできる項目を出すクラス
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+
+}
+//end basecross
