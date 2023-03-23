19df75f32057d1742c4a9aa2bf238241bdee4230
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-03-23 09:45:51 +0900

    テスト用の実行ボタンの実装

--- GameSources/RandomSelectLevelUp.cpp
+++ GameSources/RandomSelectLevelUp.cpp
@@ -27,13 +27,15 @@ namespace basecross {
 	void RandomSelectLevelUp::RandomSelect(int selectStatus)
 	{
 		int i, j; // ループ用変数
+		int selectNum = selectStatus;
 		int commandNum[m_PickUpStatusCount] = { 0 }; // 乱数格納用配列
 		srand((unsigned int)time(NULL));
 
 		for (i = 0; i < m_PickUpStatusCount; i++)
 		{
 			do { // 重複を解消
-				commandNum[i] =	rand() % selectStatus;
+				
+				commandNum[i] =	rand() % selectNum;
 				for (j = 0; j < i; j++)
 				{
 					if (commandNum[i] == commandNum[j])
@@ -51,8 +53,18 @@ namespace basecross {
 	{
 		/* プレイヤーのステータスからexpの取得量を取得
 		* 取得したexpが一定数を超えた時にRandomSelectを実行
-		* 
 		*/
+
+		// テスト用 //
+		auto& app = App::GetApp();
+		// ゲームパッドを使用する
+		auto device = app->GetInputDevice(); // インプットデバイスオブジェクトを取得する
+		auto& pad = device.GetControlerVec()[0]; // １個目のコントローラーの状態を取得する
+
+		if (pad.wPressedButtons & XINPUT_GAMEPAD_B)
+		{
+			RandomSelect(8);
+		}
 	}
 }
 //end basecross
