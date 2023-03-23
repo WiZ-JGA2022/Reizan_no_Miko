afba697d93325b9115f0419bafebe130b96be933
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-03-23 15:44:49 +0900

    GaugeSizeSetting.hと.cppを追加

---
+++ GameSources/GaugeSizeSetting.cpp
@@ -0,0 +1,11 @@
+/*!
+@file GaugeSizeSetting.cpp
+@brief 各種ゲージの頂点データなどの設定の実装
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+}
---
+++ GameSources/GaugeSizeSetting.h
@@ -0,0 +1,15 @@
+/* !
+@file GaugeSizeSetting.h
+@brief 各種ゲージの頂点データなどの設定
+*/
+
+#pragma once
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+	class GaugeSizeSetting : public GameObject 
+	{
+
+	};
+}
