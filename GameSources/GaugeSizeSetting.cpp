65c101abdb78a743188a791a16737638d67b90bb
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-03-23 16:11:44 +0900

    GaugeSizeSetting.hと.cppの内容を更新、Project.hにGaugeSizeSetting.hをインクルード

--- GameSources/GaugeSizeSetting.cpp
+++ GameSources/GaugeSizeSetting.cpp
@@ -7,5 +7,62 @@
 #include "Project.h"
 
 namespace basecross {
+	void GaugeSizeSetting::OnCreate()
+	{
+		m_gaugeWidth = m_DefaultGaugeWidth; // 1exp当たりのバーの大きさを計算
+
+		const Col4 white(1.0f, 1.0f, 1.0f, 1.0f); // 色を白(画像ファイルの色そのまま)に
+
+		float u = m_uvPosition / 10.0f; // uvの位置を調整
+
+		// 頂点データの設定
+		m_vertices = {
+			{Vec3(0.0f, 0.0f, 0.0f), white, Vec2(u, 0.0f)}, // 左上
+			{Vec3(m_gaugeWidth, 0.0f, 0.0f), white, Vec2(u + 0.1f, 0.0f)}, // 右上
+			{Vec3(0.0f,-m_DefaultGaugeHeight, 0.0f), white, Vec2(u, 1.0f)}, // 左下
+			{Vec3(m_gaugeWidth,-m_DefaultGaugeHeight, 0.0f), white, Vec2(u + 0.1f, 1.0f)}, // 右下
+		};
+
+		m_indices = {
+			0, 1, 2, // 左上の三角(ポリゴン)
+			2, 1, 3  // 右下の三角(ポリゴン)
+		};
+
+		m_draw = AddComponent<PCTSpriteDraw>(m_vertices, m_indices);
+		m_draw->SetTextureResource(L"HPBAR_GREEN");
+
+		m_transform = GetComponent<Transform>();
+
+		// 透過処理を有効にする
+		SetAlphaActive(true);
+	}
+
+	void GaugeSizeSetting::SetGaugeSize(float gaugeSizeLimit, float currentGaugeSize)
+	{
+		float lostGauge = m_gaugeWidth / gaugeSizeLimit;
+		float currentLostGauge = gaugeSizeLimit - currentGaugeSize;
+
+		for (int i = 0; i < m_SquareVartex; i++)
+		{
+			if (i % 2 == 1)
+			{
+				m_vertices[i].textureCoordinate.x = m_gaugeWidth - lostGauge * currentLostGauge;
+			}
+		}
+
+		// 更新されたデータを元にポリゴンを作り直す
+		m_draw->UpdateVertices(m_vertices);
+	}
+
+	void GaugeSizeSetting::SetPosition(const Vec3& pos, float h)
+	{
+		auto& app = App::GetApp();
+
+		// 画面の幅と高さを取得する
+		auto width = app->GetGameWidth();
+		auto height = app->GetGameHeight();
+
+		m_transform->SetPosition(Vec3(-pos.x + (width * 0.5f), -pos.y + (height * h), pos.z)); // 画面半分ずつずらして原点を画面左上にずらす。また、Y座標は下向きに反転させる
+	}
 
 }
--- GameSources/GaugeSizeSetting.h
+++ GameSources/GaugeSizeSetting.h
@@ -10,6 +10,53 @@
 namespace basecross {
 	class GaugeSizeSetting : public GameObject 
 	{
+		// HpBarの初期サイズ
+		const float m_DefaultGaugeWidth;
+		const float m_DefaultGaugeHeight;
+		// 頂点の数
+		const int m_SquareVartex;
 
+		// スプライトの元になる頂点データと頂点インデックス
+		vector<VertexPositionColorTexture> m_vertices;
+		vector<uint16_t> m_indices;
+
+		// ドローコンポーネントとトランスフォームコンポーネントを取得
+		shared_ptr<PCTSpriteDraw> m_draw;
+		shared_ptr<Transform> m_transform;
+
+		int m_uvPosition; // 表示される位置を表す値
+		float m_gaugeWidth; // 一単位毎に伸ばす・減らすゲージの横幅
+
+	public:
+		// コンストラクタ //
+		GaugeSizeSetting(const std::shared_ptr<Stage>& stage) :
+			GameObject(stage),
+			m_DefaultGaugeWidth(400.0f),
+			m_DefaultGaugeHeight(24.0f),
+			m_SquareVartex(4),
+			m_uvPosition(0),
+			m_gaugeWidth(0)
+		{
+		}
+		// 初期化 //
+		void OnCreate() override;
+
+		// UVの場所を決める関数 //
+		void SetGaugeSize(float gaugeSizeLimit, float currentGaugeSize);
+
+		// 2D座標指定でスプライトの位置を設定する関数 //
+		virtual void SetPosition(const Vec3& pos, float h);
+
+		// 2D座標指定でスプライトの位置を設定する関数（Vec2バージョン）//
+		virtual void SetPosition(const Vec2& pos, float h)
+		{
+			SetPosition(Vec3(pos.x, pos.y, 0.0f), h);
+		}
+
+		// 2D座標指定でスプライトの位置を設定する関数（floatバージョン）//
+		void SetPosition(float x, float y, float h)
+		{
+			SetPosition(Vec2(x, y), h);
+		}
 	};
 }
