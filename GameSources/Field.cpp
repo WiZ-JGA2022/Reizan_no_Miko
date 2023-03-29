33af43d3ed4d1cdc1222ccb160e7866d95fa509a
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-03-29 13:38:29 +0900

    地面の大きさを変更

--- GameSources/Field.cpp
+++ GameSources/Field.cpp
@@ -14,7 +14,7 @@ namespace basecross {
 
 		auto transComp = GetComponent<Transform>();
 		transComp->SetPosition(0.0f, -1.0f, 0.0f);
-		transComp->SetScale(10.0f, 1.0f, 5.0f);
+		transComp->SetScale(100.0f, 1.0f, 100.0f);
 	}
 
 }
