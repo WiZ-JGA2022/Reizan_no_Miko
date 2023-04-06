21f5f4981ee907d90eb79553aff629160c8afba3
yuu2611 <zeal22300025@fsgcl.jp>
2023-04-06 14:24:49 +0900

    敵の弾をプレイヤーに向かう処理を追加

--- GameSources/EnemyBullet.h
+++ GameSources/EnemyBullet.h
@@ -9,10 +9,14 @@
 namespace basecross {
 	class EnemyBullet : public GameObject
 	{
-		std::shared_ptr<Transform> m_Transform;
+		std::shared_ptr<Transform> m_transform;
 
 		float m_speed;
 		Vec3 m_forward;
+
+		Vec3 m_direction; // プレイヤーへの方向
+		Vec3 m_position;
+		Vec3 m_moveLock;
 
 		// 弾を発射したオブジェクトへのポインタ
 		std::shared_ptr<Enemy> m_owner;
@@ -21,13 +25,16 @@ namespace basecross {
 			GameObject(stage),
 			m_owner(owner),
 			m_speed(5.0f),
-			m_forward(Vec3(0))
+			m_forward(Vec3(0)),
+			m_position(Vec3(0))
 
 		{
 		}
 
 		void OnCreate() override;
 		void OnUpdate() override; // オブジェクトデータの更新
+
+		virtual Vec3 MoveEnemyBullet();
 	};
 
 
