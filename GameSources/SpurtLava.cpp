158292120fbaffa997ea6050f7687a4659bec75e
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-04-11 13:58:06 +0900

    ギミックを動的に実装できるように修正

--- GameSources/SpurtLava.cpp
+++ GameSources/SpurtLava.cpp
@@ -7,25 +7,10 @@
 #include "Project.h"
 
 namespace basecross {
-	SpurtLava::SpurtLava(const shared_ptr<Stage>& stage) :
-		GameObject(stage),
-		m_damageDelayFlame(0),
-		m_damageValue(1.0f),
-		m_scale(Vec3(5.0f, 5.0f, 5.0f)),
-		m_position(0)
-	{
-	}
-	SpurtLava::SpurtLava(const shared_ptr<Stage>& stage, const Vec3& position) :
-		GameObject(stage),
-		m_damageDelayFlame(0),
-		m_damageValue(1.0f),
-		m_scale(Vec3(5.0f, 5.0f, 5.0f)),
-		m_position(position)
-	{
-	}
 	SpurtLava::SpurtLava(const shared_ptr<Stage>& stage, const Vec3& position, const Vec3& scale) :
 		GameObject(stage),
 		m_damageDelayFlame(0),
+		m_removeDelayFlame(m_RemoveDelayCount),
 		m_damageValue(1.0f),
 		m_scale(scale),
 		m_position(position)
@@ -50,6 +35,25 @@ namespace basecross {
 	void SpurtLava::OnUpdate()
 	{
 		m_damageDelayFlame--;
+		m_removeDelayFlame--;
+		if (m_removeDelayFlame <= 0)
+		{
+			GetStage()->RemoveGameObject<SpurtLava>(GetThis<SpurtLava>());
+		}
+	}
+
+	void SpurtLava::OnCollisionEnter(shared_ptr<GameObject>& other)
+	{
+		if (other->FindTag(L"Player"))
+		{
+			if (m_damageDelayFlame <= 0)
+			{
+				auto playerStatus = GetStage()->GetSharedGameObject<PlayerStatusController>(L"PlayerStatus");
+				playerStatus->PlayerDamageProcess(m_damageValue);
+				m_damageDelayFlame = m_DamageDelayCount;
+				return;
+			}
+		}
 	}
 
 	void SpurtLava::OnCollisionExcute(shared_ptr<GameObject>& other)
