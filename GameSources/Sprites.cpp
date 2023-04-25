dfbc0e544aab32ef6c71266e4e5012d3bc79ffd3
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-04-25 09:57:46 +0900

    Sprites.hと.cppを作成

---
+++ GameSources/Sprites.cpp
@@ -0,0 +1,11 @@
+/*!
+@file Sprites.cpp
+@brief スプライトの親クラスの実装
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+}
---
+++ GameSources/Sprites.h
@@ -0,0 +1,11 @@
+/*!
+@file Sprites.h
+@brief スプライトの親クラスの実装
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+}
