162e42531ee9b14d1fd9f351e7f0f164dd0b5291
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-04-27 12:23:35 +0900

    細かい修正

--- GameSources/KeyStoneGauge.cpp
+++ GameSources/KeyStoneGauge.cpp
@@ -32,53 +32,26 @@ namespace basecross {
 			PtrTransform->SetPosition(Pos);
 			PtrTransform->SetScale(1.0f, 1.0f, 1.0f);
 			PtrTransform->SetQuaternion(SeekTransPtr->GetQuaternion());
-			//変更できるスクエアリソースを作成
-
-			//頂点配列
-			//インデックスを作成するための配列
-			//vector<uint16_t> indices;
-			////Squareの作成(ヘルパー関数を利用)
-			//MeshUtill::CreateSquare(1.0f, m_vertices, indices);
-			////左上頂点
-			//m_vertices[0].textureCoordinate = Vec2(0,0);
-			////右上頂点
-			//m_vertices[1].textureCoordinate = Vec2(1.0f, 0);
-			////左下頂点
-			//m_vertices[2].textureCoordinate = Vec2(0, 1.0f);
-			////右下頂点
-			//m_vertices[3].textureCoordinate = Vec2(1.0f, 1.0f);
-			////頂点の型を変えた新しい頂点を作成
-			//vector<VertexPositionColorTexture> new_vertices;
-			//for (auto& v : m_vertices) {
-			//	VertexPositionColorTexture nv;
-			//	nv.position = v.position;
-			//	nv.color = Col4(1.0f, 1.0f, 1.0f, 1.0f);
-			//	nv.textureCoordinate = v.textureCoordinate;
-			//	new_vertices.push_back(nv);
-			//}
-			//新しい頂点を使ってメッシュリソースの作成
-			//m_SquareMeshResource = MeshResource::CreateMeshResource<VertexPositionColorTexture>(new_vertices, indices, true);
 
 			const Col4 white(1.0f, 1.0f, 1.0f, 1.0f);
 
 			m_vertices = {
-				{Vec3(-m_DefaultSize.x * 0.5f,0.0f,0.0f),white,Vec2(0.0f,0.0f)},
-				{Vec3(m_DefaultSize.x * 0.5f,0.0f,0.0f),white,Vec2(0.1f,0.0f)},
-				{Vec3(-m_DefaultSize.x * 0.5f,-m_DefaultSize.y,0.0f),white,Vec2(0.0f,1.0f)},
-				{Vec3(m_DefaultSize.x * 0.5f,-m_DefaultSize.y,0.0f),white,Vec2(0.1f,1.0f)},
+				{Vec3(-m_DefaultSize.x * 0.5f, 0.0f			   , 0.0f), white, Vec2(0.0f, 0.0f)},
+				{Vec3(m_DefaultSize.x * 0.5f , 0.0f			   , 0.0f), white, Vec2(0.1f, 0.0f)},
+				{Vec3(-m_DefaultSize.x * 0.5f, -m_DefaultSize.y, 0.0f), white, Vec2(0.0f, 1.0f)},
+				{Vec3(m_DefaultSize.x * 0.5f , -m_DefaultSize.y, 0.0f), white, Vec2(0.1f, 1.0f)},
 			};
 			m_indices = {
-				0,1,2,
-				2,1,3
+				0, 1, 2,
+				2, 1, 3
 			};
 
 			m_draw = AddComponent<PCTStaticDraw>();
-			//m_draw->SetMeshResource(m_SquareMeshResource);
 			m_draw->SetOriginalMeshUse(true);
 			m_draw->CreateOriginalMesh(m_vertices,m_indices);
 			m_draw->SetTextureResource(L"HPBAR_GREEN");
 			SetAlphaActive(true);
-			SetDrawLayer(1);
+			SetDrawLayer((int)DrawLayer::Bottom);
 		}
 
 	}
@@ -88,25 +61,17 @@ namespace basecross {
 	void KeyStoneGauge::OnUpdate() {
 
 		if (!m_keyStoneHp.expired()) {
-			auto SeekPtr = m_keyStoneHp.lock();
-			auto SeekTransPtr = SeekPtr->GetComponent<Transform>();
 
-			auto PtrTransform = GetComponent<Transform>();
+			auto transform = GetComponent<Transform>();
+			auto stonePtr = m_keyStoneHp.lock();
+			auto cameraPtr = GetStage()->GetView()->GetTargetCamera();
+			// ゲージの大きさを更新
+			UpdateGaugeSize(stonePtr->GetMaxHp(), stonePtr->GetCurrentHp());
 
-			auto stone = GetStage()->GetSharedGameObject<KeyStone>(L"KeyStone");
-			UpdateGaugeSize(stone->GetMaxHp(), stone->GetCurrentHp());
-			//m_draw->UpdateVertices(m_vertices);
-
-
-			auto PtrCamera = GetStage()->GetView()->GetTargetCamera();
-
-
-			Quat Qt;
 			//向きをビルボードにする
-			Qt = Billboard(PtrCamera->GetAt() - PtrCamera->GetEye());
-
-			PtrTransform->SetQuaternion(Qt);
-
+			Quat Qt;
+			Qt = Billboard(cameraPtr->GetAt() - cameraPtr->GetEye());
+			transform->SetQuaternion(Qt);
 		}
 
 	}
