1f7828ae67de5ca4d34897969b584ec82ca21ed6
yuu2611 <zeal22300025@fsgcl.jp>
2023-05-01 15:27:49 +0900

    EnemySpawnクラスを生成

---
+++ GameSources/EnemySpawn.cpp
@@ -0,0 +1,13 @@
+/*!
+@file EnemySpawn.cpp
+@brief 敵の出現パターンなど
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+
+}
+//end basecross
--- GameSources/EnemySpawn.h
+++ GameSources/EnemySpawn.h
@@ -1,1 +1,21 @@
+/*!
+@file EnemyController.h
+@brief “G‚È‚Ç
+*/
+
 #pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class EnemySpawn :public GameObject
+	{
+
+
+	public:
+		EnemySpawn(const shared_ptr<Stage>& stage) :
+			GameObject(stage)
+		{
+		}
+	};
+
+}
