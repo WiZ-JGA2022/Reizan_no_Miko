0d1b583ae5969ecd4d4ea0881f37938d0a257b29
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-05-01 18:13:36 +0900

    ステージを更新

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -118,7 +118,7 @@ namespace basecross{
 		RoadSound(L"LAVA_SE", L"StaticLavaSE.wav");
 		RoadSound(L"SPURTLAVA_SE", L"SpurtLavaSE.wav");
 
-		RoadStaticModelMesh(L"atage3", L"STAGE");
+		RoadStaticModelMesh(L"O_stage", L"STAGE");
 		RoadStaticModelMesh(L"tyokore-toke-ki", L"BOX");
 		RoadStaticModelMesh(L"Holl2", L"ARCH");
 
