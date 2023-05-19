22f9bf7c733933ed5e2757dec647e604cf3b0135
Tomoki Suzuki <zeal22300014@fsgcl.jp>
2023-05-19 16:13:09 +0900

    aaa

--- GameSources/RBButtonUI.cpp
+++ GameSources/RBButtonUI.cpp
@@ -10,17 +10,26 @@
 namespace basecross {
 	RBButtonSprite::RBButtonSprite(const shared_ptr<Stage>& stage) :
 		Sprites(stage),
-		m_DefaultSize(Vec2(150.0f, 150.0f))
+		m_DefaultSize(Vec2(150.0f, 150.0f)),
+		m_AfterSize(Vec2(170.0f, 170.0f))
 	{
 	}
 	RBButtonSprite::~RBButtonSprite() {}
 
 	void RBButtonSprite::OnCreate()
 	{
-		Sprites::CreateSprite(Vec3(400.0f, 350.0f, 0.0f), m_DefaultSize, L"RB_BUTTON");
+		Sprites::CreateSeekSizeSprite(Vec3(400.0f, 350.0f, 0.0f), m_DefaultSize,m_AfterSize, L"RB_BUTTON", SeekType::BottomRight,5.0f);
 	}
 
 	void RBButtonSprite::OnUpdate()
 	{
+		auto& app = App::GetApp();
+		auto device = app->GetInputDevice();
+		auto& pad = device.GetControlerVec()[0];
+		if ((pad.wPressedButtons && RB_BUTTON) && (m_isSeekSizeState != RBSeekSizeState::Stop))
+		{
+			m_isSeekSizeState = RBSeekSizeState::SizeChange;
+			Sprites::OnUpdate();
+		}
 	}
 }
