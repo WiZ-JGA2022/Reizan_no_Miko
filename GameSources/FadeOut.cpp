b18c467ca7944aa27a5495c10fb4f90b9fe0f9a7
Yuki Haga <zeal22300182@fsgcl.jp>
2023-06-09 14:21:00 +0900

    コメントの追加

--- GameSources/FadeOut.cpp
+++ GameSources/FadeOut.cpp
@@ -8,6 +8,7 @@
 #include "Project.h"
 
 namespace basecross {
+	// コンストラクタとデストラクタ
 	FadeOut::FadeOut(const shared_ptr<Stage>& stage, const wstring& texKey) :
 		Sprites(stage),
 		m_DefaultSize(Vec2((float)App::GetApp()->GetGameWidth(), (float)App::GetApp()->GetGameHeight())),
@@ -15,9 +16,11 @@ namespace basecross {
 	{
 	}
 	FadeOut::~FadeOut() {}
+	// ここまで
 
 	void FadeOut::OnCreate()
 	{
+		// 初期値の設定(縦、高さ、横)
 		Sprites::CreateFadeSprite(Vec3(-m_DefaultSize.x / 2, m_DefaultSize.y / 2, 0.0f), m_DefaultSize, m_texKey, FadeType::FadeOut);
 	}
 
